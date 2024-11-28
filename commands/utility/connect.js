3e0d6a1c722d24efddb87c2e9abfc8a7093014d1
Léo-Paul HUAR <leopaul.huar@gmail.com>
2024-11-28 16:39:50 +0100

    connect to voice channel

---
+++ commands/utility/connect.js
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { joinVoiceChannel } from '@discordjs/voice';
+
+export const data = new SlashCommandBuilder()
+	.setName('connect')
+	.setDescription('Connects the bot to your current voice channel.');
+
+export async function execute(interaction) {
+	// Vérifie si l'utilisateur est dans un salon vocal
+	const memberVoiceChannel = interaction.member.voice.channel;
+
+	if (!memberVoiceChannel) {
+		// Si l'utilisateur n'est pas dans un salon vocal
+		await interaction.reply({
+			content: 'You need to be in a voice channel to use this command!',
+			ephemeral: true, // Message visible uniquement par l'utilisateur
+		});
+		return;
+	}
+
+	// Connecte le bot au salon vocal
+	try {
+		const connection = joinVoiceChannel({
+			channelId: memberVoiceChannel.id,
+			guildId: memberVoiceChannel.guild.id,
+			adapterCreator: memberVoiceChannel.guild.voiceAdapterCreator,
+		});
+
+        console.log(`Connected to ${memberVoiceChannel.name}!`)
+		await interaction.reply(`Connected to ${memberVoiceChannel.name}!`);
+	} catch (error) {
+		console.error(`[ERROR] Failed to connect: ${error}`);
+		await interaction.reply({
+			content: 'An error occurred while trying to connect to the voice channel.',
+			ephemeral: true,
+		});
+	}
+}
