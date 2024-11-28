558bd6b5d82c0d087e9379992326ea6767d00216
Léo-Paul HUAR <leopaul.huar@gmail.com>
2024-11-28 16:20:22 +0100

    add error control to ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,6 +3,15 @@ import { SlashCommandBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
 	.setName('ping')
 	.setDescription('Replies with Pong!');
+
 export async function execute(interaction) {
-	await interaction.reply('Pong!');
+	try {
+		await interaction.reply('Pong!');
+	} catch (error) {
+		console.error(`[ERROR] Failed to execute "ping" command: ${error}`);
+		await interaction.reply({
+			content: 'Something went wrong while executing this command.',
+			ephemeral: true,
+		});
+	}
 }
